49d7dfd214dedcd60ff9581adea4427d63af878c
Dev Verma <devverma9451@gmail.com>
2026-09-25 15:19:06 +0530

    update

--- app.py
+++
@@ -1,3 +0,0 @@
-"""Backward-compatible Streamlit entry point."""
-
-from frontend.app import *  # noqa: F401,F403
--- dashboard/metrics.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict
-
-def compute_manual_metrics(text: str, page_count: int) -> Dict:
-    words = len(text.split())
-    return {'pages': page_count, 'words': words}
